94147d67adb1204b1661b778113ab1ed8d72e715
Son-Guhun <criowoc@hotmail.com>
2018-04-03 02:30:29 -0300

    Added options

--- setup.py
+++ setup.py
@@ -8,4 +8,7 @@ Created on Fri Mar 30 00:34:46 2018
 from distutils.core import setup
 import py2exe
  
-setup(console=['SaveNLoad.py']);
+setup(console=['SaveNLoad.py']
+          #,options = {'py2exe': {'bundle_files': 1, 'compressed': True}}
+          #,zipfile = None
+          );
