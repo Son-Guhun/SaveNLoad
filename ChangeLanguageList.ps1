6b96207b8f8cb640ffa7fe622c574a265d3b6a5c
Son-Guhun <criowoc@hotmail.com>
2018-04-09 02:26:22 -0300

    Added try/finally blocks
    Possibly avoid some kind of error? Better safe than sorry.

--- ChangeLanguageList.ps1
+++ ChangeLanguageList.ps1
@@ -1,6 +1,12 @@
 ipmo international
 $oldLangList = Get-WinUserLanguageList
 Set-WinUserLanguageList -Force 'en-US'
-echo 'User languague sucessfully changed to en-US'
-$PIPE = Read-Host
-Set-WinUserLanguageList -Force $oldLangList
+Try
+{
+    echo 'User languague sucessfully changed to en-US'
+    $PIPE = Read-Host
+}
+Finally
+{
+    Set-WinUserLanguageList -Force $oldLangList
+}
